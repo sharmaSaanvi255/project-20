dfac7b9a9436b764eac90715c81582fc44cdfeea
sharmaSaanvi255 <72978396+sharmaSaanvi255@users.noreply.github.com>
2020-10-31 22:25:21 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
 var car,wall;
 var speed,weight;
+var touras,cyclap,zenia;
+var deformation;
 
 
 function setup() {
@@ -15,13 +17,27 @@ function setup() {
 
  wall=createSprite(1000,200,60,height/2);
  wall.shapeColor="red";
-
-  if ()
 }
 
 function draw() {
-  background(200); 
+  background(200);
+  
+  if(wall.x-car.x < (wall.width+ car.width)/2){
+  car.velocityX=0;
+  deformation=0.5*weight*speed*speed/2250;
+  }
+
+  if (deformation>180) {
+    car.shapeColor="magenta";
+  } else if (deformation>100 && deformation<180)
+   {
+    car.shapeColor="purple";
+  } else (deformation<100)
+   {
+    car.shapeColor="pink";
+  }
+  drawSprites();
+  }
   
    
-  drawSprites();
-}
+  
